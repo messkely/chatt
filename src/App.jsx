23e392312833f352a719566e3843c95cb5855d27
23jfk <momomido712@gmail.com>
2023-06-11 00:17:56 +0100

    second Day

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
-    <>
-    </>
-  )
-}
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
-export default App
+
+export default App;
